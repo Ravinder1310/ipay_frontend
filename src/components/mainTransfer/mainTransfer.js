4b5d62593520778efb3e5ffa409996935dec54c3
Ravinder1310 <105969256+Ravinder1310@users.noreply.github.com>
2024-10-07 10:00:33 +0530

    added

--- src/components/mainTransfer/mainTransfer.js
+++ src/components/mainTransfer/mainTransfer.js
@@ -2,54 +2,54 @@ import React from "react";
 
 const MainTransfer = () => {
   return (
-    <div className="w-[90%] m-auto mt-10 bg-transparent">
+    <div className="w-[95%] m-auto mt-10 bg-transparent">
       <h1 className="text-left mb-3 text-2xl font-bold text-white">
         Money Transfer
       </h1>
-      <div className="flex flex-wrap border-2 rounded-lg gap-4 bg-blue-300 shadow-2xl justify-between p-4">
-        <div className="hover:bg-gray-200 p-2 rounded-lg hover:cursor-pointer w-[120px] h-[130px] flex flex-col items-center justify-center">
+      <div className="flex flex-wrap border-2 rounded-lg gap-4 bg-blue-300 shadow-2xl justify-between px-1 py-4">
+        <div className="hover:bg-gray-200 p-2 rounded-lg  hover:cursor-pointer w-[100px] h-[100px] flex flex-col items-center justify-center">
           <img
-            className="w-[70px] h-[60px] m-auto"
+            className="w-[40px] h-[40px] m-auto"
             src="/images/add_money.png"
             alt="error"
           />
-          <h4>Add Money</h4>
+          <h4 className="">Add Money</h4>
         </div>
-        <div className="hover:bg-gray-200 p-2 rounded-lg hover:cursor-pointer w-[120px] h-[130px] flex flex-col items-center justify-center">
+        <div className="hover:bg-gray-200 p-2 rounded-lg  hover:cursor-pointer w-[100px] h-[100px] flex flex-col items-center justify-center">
           <img
-            className="w-[70px] h-[60px] m-auto"
+            className="w-[40px] h-[40px] m-auto"
             src="/images/send_money.png"
             alt="error"
           />
           <h4>Send Money</h4>
         </div>
-        <div className="hover:bg-gray-200 p-2 rounded-lg hover:cursor-pointer w-[120px] h-[130px] flex flex-col items-center justify-center">
+        <div className="hover:bg-gray-200 p-2 rounded-lg  hover:cursor-pointer w-[100px] h-[100px] flex flex-col items-center justify-center">
           <img
-            className="w-[70px] h-[60px] m-auto"
+            className="w-[40px] h-[40px] m-auto"
             src="/images/scan_pay.png"
             alt="error"
           />
           <h4>Scan to Pay</h4>
         </div>
-        <div className="hover:bg-gray-200 p-2 rounded-lg hover:cursor-pointer w-[120px] h-[130px] flex flex-col items-center justify-center">
+        <div className="hover:bg-gray-200 p-2 rounded-lg hover:cursor-pointer w-[100px] h-[100px] flex flex-col items-center justify-center">
           <img
-            className="w-[70px] h-[60px] m-auto"
+            className="w-[40px] h-[40px] m-auto"
             src="/images/upi.png"
             alt="error"
           />
           <h4>Pay to UPI</h4>
         </div>
-        <div className="hover:bg-gray-200 p-2 rounded-lg hover:cursor-pointer w-[120px] h-[130px] flex flex-col items-center justify-center">
+        <div className="hover:bg-gray-200 p-2 rounded-lg hover:cursor-pointer w-[100px] h-[100px] flex flex-col items-center justify-center">
           <img
-            className="w-[70px] h-[60px] m-auto"
+            className="w-[40px] h-[40px] m-auto"
             src="/images/redeem_money.png"
             alt="error"
           />
           <h4>Redeem Money</h4>
         </div>
-        <div className="hover:bg-gray-200 p-2 rounded-lg hover:cursor-pointer w-[120px] h-[130px] flex flex-col items-center justify-center">
+        <div className="hover:bg-gray-200 p-2 rounded-lg hover:cursor-pointer w-[100px] h-[100px] flex flex-col items-center justify-center">
           <img
-            className="w-[70px] h-[60px] m-auto"
+            className="w-[40px] h-[40px] m-auto"
             src="/images/upi.png"
             alt="error"
           />
--- src/components/recharge&BillPay/recharge&BillPay.js
+++ src/components/recharge&BillPay/recharge&BillPay.js
@@ -19,108 +19,108 @@ const RechargeAndBill = () => {
   };
 
   return (
-    <div className="w-[90%] m-auto mt-10">
+    <div className="w-[95%] m-auto mt-10">
       {/* Include Toaster here */}
       <Toaster />
 
       <h1 className="text-left mb-3 text-2xl font-bold text-white">Recharge & Bill Pay</h1>
-      <div className="flex flex-wrap border-2 rounded-lg gap-4 gap-x-5 bg-blue-300 shadow-2xl justify-between text-sm font-bold p-4 px-6">
+      <div className="flex flex-wrap border-2 rounded-lg gap-y-10 gap-x-4 bg-blue-300 shadow-2xl justify-center text-sm font-bold py-4 px-2">
         <div
-          className="hover:bg-gray-200 p-2 rounded-lg hover:cursor-pointer w-[127px] h-[150px] flex flex-col items-center justify-center"
+          className="hover:bg-gray-200 rounded-lg border hover:cursor-pointer w-[90px] h-[120px] flex flex-col items-center justify-center"
           onClick={() => {
             navigate("/recharge-mobile");
           }}
         >
           <img
-            className="w-[70px] h-[60px] m-auto"
+            className="w-[40px] h-[40px] m-auto"
             src="/images/mobile_recharge.png"
             alt="Mobile Recharge"
           />
-          <h4 className="mt-4">Mobile Recharge</h4>
+          <h4 className="mb-2">Mobile Recharge</h4>
         </div>
 
         <div
-          className="hover:bg-gray-200 p-2 rounded-lg hover:cursor-pointer w-[120px] h-[150px] flex flex-col items-center justify-center"
+          className="hover:bg-gray-200 p-2 rounded-lg border hover:cursor-pointer w-[90px] h-[120px] flex flex-col items-center justify-center"
           onClick={handleToast}
         >
           <img
-            className="w-[70px] h-[60px] m-auto"
+            className="w-[40px] h-[40px] m-auto"
             src="/images/gas_cylinder.png"
             alt="Gas Cylinder"
           />
-          <h4 className="mt-4">Gas Cylinder</h4>
+          <h4 className="mb-2">Gas Cylinder</h4>
         </div>
 
         <div
-          className="hover:bg-gray-200 p-2 rounded-lg hover:cursor-pointer w-[127px] h-[150px] flex flex-col items-center justify-center"
+          className="hover:bg-gray-200 p-2 border rounded-lg hover:cursor-pointer w-[90px] h-[120px] flex flex-col items-center justify-center"
           onClick={handleToast}
         >
           <img
-            className="w-[70px] h-[60px] m-auto"
+            className="w-[40px] h-[40px] m-auto"
             src="/images/water_bill.png"
             alt="Water Bill"
           />
-          <h4 className="mt-4">Water Bill</h4>
+          <h4 className="mb-2">Water Bill</h4>
         </div>
 
         <div
-          className="hover:bg-gray-200 p-2 rounded-lg hover:cursor-pointer w-[120px] h-[150px] flex flex-col items-center justify-center"
+          className="hover:bg-gray-200 p-2 border rounded-lg hover:cursor-pointer w-[90px] h-[120px] flex flex-col items-center justify-center"
           onClick={handleToast}
         >
           <img
-            className="w-[70px] h-[60px] m-auto"
+            className="w-[40px] h-[40px] m-auto"
             src="/images/electric_bill.png"
             alt="Electricity Bill"
           />
-          <h4 className="mt-4">Electricity Bill</h4>
+          <h4 className="mb-2">Electricity Bill</h4>
         </div>
 
         <div
-          className="hover:bg-gray-200 p-2 rounded-lg hover:cursor-pointer w-[127px] h-[150px] flex flex-col items-center justify-center"
+          className="hover:bg-gray-200 p-2 border rounded-lg hover:cursor-pointer w-[90px] h-[120px] flex flex-col items-center justify-center"
           onClick={handleToast}
         >
           <img
-            className="w-[70px] h-[60px] m-auto"
+            className="w-[40px] h-[40px] m-auto"
             src="/images/dth_bill.png"
             alt="DTH Recharge"
           />
-          <h4 className="mt-4">DTH Recharge</h4>
+          <h4 className="mb-2">DTH Recharge</h4>
         </div>
 
         <div
-          className="hover:bg-gray-200 p-2 rounded-lg hover:cursor-pointer w-[120px] h-[150px] flex flex-col items-center justify-center"
+          className="hover:bg-gray-200 p-2 border rounded-lg hover:cursor-pointer w-[90px] h-[120px] flex flex-col items-center justify-center"
           onClick={handleToast}
         >
           <img
-            className="w-[70px] h-[60px] m-auto"
+            className="w-[40px] h-[40px] m-auto"
             src="/images/fastag.png"
             alt="FasTag"
           />
-          <h4 className="mt-4">FasTag</h4>
+          <h4 className="mb-2">FasTag</h4>
         </div>
 
         <div
-          className="hover:bg-gray-200 p-2 rounded-lg hover:cursor-pointer w-[127px] h-[150px] flex flex-col items-center justify-center"
+          className="hover:bg-gray-200 p-2 border rounded-lg hover:cursor-pointer w-[90px] h-[120px] flex flex-col items-center justify-center"
           onClick={handleToast}
         >
           <img
-            className="w-[70px] h-[60px] m-auto"
+            className="w-[40px] h-[40px] m-auto"
             src="/images/loan_emi.png"
             alt="Loan & EMI"
           />
-          <h4 className="mt-4">Loan & EMI</h4>
+          <h4 className="mb-2">Loan & EMI</h4>
         </div>
 
         <div
-          className="hover:bg-gray-200 p-2 rounded-lg hover:cursor-pointer w-[120px] h-[150px] flex flex-col items-center justify-center"
+          className="hover:bg-gray-200 p-2 border rounded-lg hover:cursor-pointer w-[90px] h-[120px] flex flex-col items-center justify-center"
           onClick={handleToast}
         >
           <img
-            className="w-[70px] h-[60px] m-auto"
+            className="w-[40px] h-[40px] m-auto"
             src="/images/gas_line.png"
             alt="Piped Gas Bill"
           />
-          <h4 className="mt-4">Piped Gas Bill</h4>
+          <h4 className="mb-2">Piped Gas Bill</h4>
         </div>
       </div>
     </div>
